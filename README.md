b286685678e2827e7ce89858526759fc3b6c9cf1
thcathy <thcathy@gmail.com>
2015-12-19 23:59:51 +0800

    Get Day 8 second star update project and README

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-Advent of Code
-==============
+# Solutions to [Advent Of Code](http://adventofcode.com/)
 
 Advent of Code is a series of small programming puzzles for a variety of skill levels. They are self-contained and are just as appropriate for an expert who wants to stay sharp as they are for a beginner who is just learning to code. Each puzzle calls upon different skills and has two parts that build on a theme.
 
-I am sharing my source in case any one interested to the solution written by Java.
-I also adapting the functional way to solving the question.
+I am sharing my sources which are written in Java 8 to anyone who is interested. I also adapting the functional way to solving the question.
+Some of my answers might not have the optimal performance but I wanted to keep the code readable rather than compact and fast.
+
+## Author
+
+Timmy Wong (thcathy@gmail.com)
--- src/main/java/com/adventofcode/Day8.java
+++ src/main/java/com/adventofcode/Day8.java
@@ -1,41 +1,134 @@
-package com.adventofcode
+package com.adventofcode;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 
 public class Day8 {
+	final static String inputFile = "day8_1.txt";
+	final static String testFile = "day8_test.txt";
+	final static Logger log = LoggerFactory.getLogger(Day8.class);
+	
+	public static void main(String... args) throws Exception {
+		test1();
+		firstStar();
+		
+		test2();
+		secondStar();		
+	}
+	
+	static void test1() throws Exception {
+		List<String> input = readInputFromFile(testFile);
+		
+		int totalMemorySize = input.stream()
+								.mapToInt(String::length)
+								.sum();
+		
+		int totalRealSize = input.stream()
+								.mapToInt(Day8::calculateRealSize)
+								.sum();
+		
+		log.info("[test1] Total Memory Size: {}, total real size: {}, difference: {}", totalMemorySize, totalRealSize, totalMemorySize - totalRealSize);
+	}
+	
+	static void test2() throws Exception {
+		List<String> input = readInputFromFile(testFile);
+		List<String> transformed = input.stream()
+			.map(Day8::transform)
+			.collect(Collectors.toList());
+		
+		int totalMemorySize = transformed.stream()
+				.mapToInt(String::length)
+				.sum();
 
-	public static void main(String[] args) {
-		int literalCount = 0;
-		int treatedCount = 0;
-		String line;
-		StringBuilder sb = null;
-		try (BufferedReader br = new BufferedReader(new FileReader(new File(
-				"day8_1.txt")))) {
-			while ((line = br.readLine()) != null) {
-				sb = new StringBuilder(line);
-				literalCount += sb.length();
-				// for every escape char we append a slash before it
-				for (int i = 0; i < sb.length(); i++) {
-					char c = sb.charAt(i);
-					if (c == '"' || c == '\\') {
-						// we use the StringBuilder to insert the slash into it
-						// and we add 1 to i because the string has a new char
-						// in it
-						sb.replace(i, i + 1, "\\" + c);
-						i++;
-					}
-				}
-				// +2 because the final string is surrounded again by quotes
-				treatedCount += sb.length() + 2;
+		int totalRealSize = transformed.stream()
+				.mapToInt(Day8::calculateRealSize)
+				.sum();
+
+		log.info("[test2] Total Memory Size: {}, total real size: {}, difference: {}", totalMemorySize, totalRealSize, totalMemorySize - totalRealSize);
+	}
+		
+	static void secondStar() throws Exception {
+		List<String> input = readInputFromFile(inputFile);
+		List<String> transformed = input.stream()
+			.map(Day8::transform)
+			.collect(Collectors.toList());
+		
+		int totalMemorySize = transformed.stream()
+				.mapToInt(String::length)
+				.sum();
+
+		int totalRealSize = transformed.stream()
+				.mapToInt(Day8::calculateRealSize)
+				.sum();
+
+		log.info("[secondStar] Total Memory Size: {}, total real size: {}, difference: {}", totalMemorySize, totalRealSize, totalMemorySize - totalRealSize);
+	}	
+		
+	static void firstStar() throws Exception {
+		List<String> input = readInputFromFile(inputFile);
+		
+		int totalMemorySize = input.stream()
+								.mapToInt(String::length)
+								.sum();
+		
+		int totalRealSize = input.stream()
+								.mapToInt(Day8::calculateRealSize)
+								.sum();
+		
+		log.info("[firstStar] Total Memory Size: {}, total real size: {}, difference: {}", totalMemorySize, totalRealSize, totalMemorySize - totalRealSize);
+	}
+	
+	static int calculateRealSize(String s) {
+		char[] charArr = s.substring(1, s.length()-1).toCharArray();
+		
+		int size=0, pos=0;
+		while (pos < charArr.length) {
+			if (charArr[pos] == '\\' && pos+1 < charArr.length && charArr[pos+1] == 'x') {
+				pos += 4;
+			} else if (charArr[pos] == '\\') {
+				pos += 2;
+			} else {
+				pos += 1;
 			}
-			System.out.println(treatedCount - literalCount);
-
-		} catch (IOException e) {
-
+			size++;
 		}
-
+		
+		//log.debug("String '{}' real size is {}", s, size);
+		return size;
+	}
+	
+	static String transform(String s) {
+		StringBuffer sb = new StringBuffer("\"\\\"");
+		
+		char[] charArr = s.substring(1, s.length()-1).toCharArray();
+		int pos=0;
+		while (pos < charArr.length) {
+			sb.append(charArr[pos]);
+			
+			if (charArr[pos] == '\\' && pos+1 < charArr.length && charArr[pos+1] == 'x') {
+				sb.append("\\");
+			} else if (charArr[pos] == '\\' && pos+1 < charArr.length && charArr[pos+1] == '\\') {
+				sb.append("\\\\\\");
+				pos++;
+			} else if (charArr[pos] == '\\') {
+				sb.append("\\\\");
+			}
+			
+			pos++;
+		}
+		sb.append("\\\"\"");
+		log.debug("{} is transformed to {}", s, sb.toString());
+		return sb.toString();
+	}
+	
+	private static List<String> readInputFromFile(String filename) throws IOException {
+		return Resources.readLines(Resources.getResource(filename), Charsets.UTF_8);
 	}
 }
