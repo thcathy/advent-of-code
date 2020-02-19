80adc9908ad0206887901ac4d4a92b2e27662bd9
Timmy Wong <timmy.wong@clsa.com>
2020-02-19 10:40:34 +0800

    finish 2019 day19

---
+++ src/main/java/com/adventofcode/year2019/Day19Part2.java
@@ -0,0 +1,260 @@
+package com.adventofcode.year2019;
+
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+public class Day19Part2 {
+    static Logger log = LoggerFactory.getLogger(Day19Part2.class);
+    final static String inputFile = "2019/day19_1.txt";
+    final static int INVALID_ADDRESS = -1;
+    final static long STOP_PROGRAM_SINGAL = 99;
+
+    public static void main(String... args) throws IOException {
+        Day19Part2 solution = new Day19Part2();
+        solution.run();
+    }
+
+    void run() throws IOException {
+        var lines = Resources.readLines(ClassLoader.getSystemResource(inputFile), Charsets.UTF_8);
+        var program = convertToLongArray(lines.get(0));
+        var beams = new HashSet<Position>();
+        var width = 1000;   // hard coded by printing the map
+        var requiredSize = 100;
+        var y = requiredSize;
+        Optional<Position> result = Optional.empty();
+        while (!result.isPresent()) {
+            queryBeamOnNextLine(program, beams, y);
+            result = canBeamFitPositionFound(beams, requiredSize, y);
+            y++;
+        }
+        log.warn("What value do you get if you take that point's X coordinate, multiply it by 10000, then add the point's Y coordinate? {}", result.get().x * 10000 + result.get().y);
+    }
+
+    private void queryBeamOnNextLine(long[] program, HashSet<Position> beams, int y) {
+        long x=0;
+        boolean hasFirstBeam = false, stopped = false;
+        while (!stopped) {
+            if (positionIsBeam(x, y, program)) {
+                beams.add(new Position(x, y));
+                hasFirstBeam = true;
+            } else {
+                if (hasFirstBeam) stopped = true;
+            }
+            x++;
+        }
+    }
+
+    Optional<Position> canBeamFitPositionFound(Set<Position> beams, int size, int bottomY) {
+        if (bottomY < size) return Optional.empty();
+
+        var leftX = 0;
+        while (!beams.contains(new Position(leftX, bottomY)))
+            leftX++;
+        var rightX = leftX + size - 1;
+        var topY = bottomY - size + 1;
+        var bottomRightPosition = new Position(rightX, bottomY);
+        var topRightPosition = new Position(rightX, topY);
+        if (beams.contains(bottomRightPosition) && beams.contains(topRightPosition))
+            return Optional.of(new Position(leftX, topY));
+        else
+            return Optional.empty();
+    }
+
+    boolean positionIsBeam(long x, long y, long[] program) {
+        var software = new Software(program, List.of(x, y));
+        executeProgram(software);
+        return software.output.get(0) == 1;
+    }
+
+    long[] convertToLongArray(String input) {
+        return Arrays.stream(input.split(","))
+                .mapToLong(Long::valueOf)
+                .toArray();
+    }
+
+    void executeProgram(Software software) {
+        while (!software.isFinished()) {
+            getOperationType(String.valueOf(software.getOpCode())).apply.accept(software);
+        }
+    }
+
+    OperationType getOperationType(String opCode) {
+        String code = opCode.length() == 1 ? opCode : opCode.substring(opCode.length() - 1);
+        for (OperationType operation : OperationType.values()) {
+            if (code.equals(operation.code)) return operation;
+        }
+        throw new RuntimeException("cannot find operation type from opcode " + opCode);
+    }
+
+    enum OperationType {
+        Addition("1", (software) -> {
+            var value = getParamValue(software, 1) + getParamValue(software, 2);
+            var outputPosition = getOutputPosition(software, 3);
+            software.program.put(outputPosition, value);
+            software.increasePointer(4);
+        }),
+        Multiply("2", (software) -> {
+            var value = getParamValue(software, 1) * getParamValue(software, 2);
+            var outputPosition = getOutputPosition(software, 3);
+            software.program.put(outputPosition, value);
+            software.increasePointer(4);
+        }),
+        Input("3", (software) -> {
+            var outputPosition = getOutputPosition(software, 1);
+            software.program.put(outputPosition, software.input.remove(0));
+            software.increasePointer(2);
+        }),
+        Output("4", (software) -> {
+            var value = getParamValue(software, 1);
+            software.output.add(value);
+            software.increasePointer(2);
+        }),
+        JumpIfTrue("5", (software) -> {
+            if (getParamValue(software, 1) != 0)
+                software.pointer = getParamValue(software, 2);
+            else
+                software.increasePointer(3);
+        }),
+        JumpIfFalse("6", (software) -> {
+            if (getParamValue(software, 1) == 0)
+                software.pointer = getParamValue(software, 2);
+            else
+                software.increasePointer(3);
+        }),
+        LessThan("7", (software) -> {
+            var value = (getParamValue(software, 1) < getParamValue(software, 2)) ? 1l : 0l;
+            var outputPosition = getOutputPosition(software, 3);
+            software.program.put(outputPosition, value);
+            software.increasePointer(4);
+        }),
+        Equals("8", (software) -> {
+            var value = (getParamValue(software, 1) == getParamValue(software, 2)) ? 1l : 0l;
+            var outputPosition = getOutputPosition(software, 3);
+            software.program.put(outputPosition, value);
+            software.increasePointer(4);
+        }),
+        AdjustRelativeBase("9", (software) -> {
+            software.relativeBase += getParamValue(software, 1);
+            software.increasePointer(2);
+        });
+
+        String code;
+        Consumer<Software> apply;
+
+        OperationType(String code, Consumer<Software> apply) {
+            this.code = code;
+            this.apply = apply;
+        }
+    }
+
+    class Software {
+        Map<Long, Long> program = new HashMap<>();
+        List<Long> input;
+        List<Long> output;
+        long pointer;
+        long relativeBase = 0;
+
+        Software(long[] program, Collection<Long> inputs) {
+            setProgram(program);
+            this.input = new ArrayList<>(inputs);
+            this.output = new ArrayList<>();
+            this.pointer = 0;
+        }
+
+        void setProgram(long[] input) {
+            for (int i = 0; i < input.length; i++) {
+                program.put(Long.valueOf(i), input[i]);
+            }
+        }
+
+        boolean isFinished() {
+            return STOP_PROGRAM_SINGAL == program.get(pointer);
+        }
+
+        long getOpCode() { return program.get(pointer); }
+
+        void increasePointer(int i) { pointer += i; }
+    }
+
+    static String getMode(long input, int paramPosition) {
+        var inputString = String.valueOf(input);
+        var value = "0";
+        if (inputString.length() >= paramPosition + 2) {
+            value = inputString.substring(inputString.length() - 2 - paramPosition, inputString.length() - 1 - paramPosition);
+        }
+        return value;
+    }
+
+    static long getParamValue(Software software, int paramPosition) {
+        long value = software.program.get(software.pointer + paramPosition);
+        var mode = getMode(software.getOpCode(), paramPosition);
+        if ("1".equals(mode))
+            return value;
+
+        long position = INVALID_ADDRESS;
+        if ("0".equals(mode))
+            position = software.program.containsKey(value) ? value : INVALID_ADDRESS;
+        else if ("2".equals(mode))
+            position = software.program.containsKey(software.relativeBase + value) ? software.relativeBase + value : INVALID_ADDRESS;
+
+        return (position != INVALID_ADDRESS) ? software.program.get(position) : 0;
+    }
+
+    static long getOutputPosition(Software software, int paramPosition) {
+        long value = software.program.get(software.pointer + paramPosition);
+        var mode = getMode(software.getOpCode(), paramPosition);
+
+        return value + ("2".equals(mode) ? software.relativeBase : 0);
+    }
+
+    class Position {
+        long x = 0, y = 0;
+
+        public Position(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() { return MessageFormat.format("({0},{1})", x, y); }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Position position = (Position) o;
+            return new EqualsBuilder()
+                    .append(x, position.x)
+                    .append(y, position.y)
+                    .isEquals();
+        }
+
+        @Override
+        public int hashCode() {
+            return new HashCodeBuilder(17, 37)
+                    .append(x)
+                    .append(y)
+                    .toHashCode();
+        }
+    }
+
+}
