1c6b6ce47b380e6b1ef2424409482663a311799d
Timmy Wong <thcathy@gmail.com>
2022-06-20 21:25:43 +0800

    finish 2017 Day12

---
+++ src/main/java/com/adventofcode/year2017/Day12Part1.java
@@ -0,0 +1,54 @@
+package com.adventofcode.year2017;
+
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Day12Part1 {
+    final static String inputFile = "2017/day12_1.txt";
+
+    public static void main(String... args) throws IOException {
+        Day12Part1 solution = new Day12Part1();
+        solution.run();
+    }
+
+    void run() throws IOException {
+        var lines = Resources.readLines(ClassLoader.getSystemResource(inputFile), Charsets.UTF_8);
+        var result = groupSize(programMap(lines), 0);
+        System.out.printf("How many programs are in the group that contains program ID 0? %d %n", result);
+    }
+
+    int groupSize(Map<Integer, Set<Integer>> programMap, int programId) {
+        Set<Integer> group = new HashSet<>();
+        List<Integer> programs = new LinkedList<>();
+        programs.add(programId);
+        while (!programs.isEmpty()) {
+            var program = programs.remove(0);
+            if (!group.contains(program)) {
+                group.add(program);
+                programs.addAll(programMap.get(program));
+            }
+        }
+        return group.size();
+    }
+
+    Map<Integer, Set<Integer>> programMap(List<String> inputs) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (String input : inputs) {
+            var programs = input.split(" <-> ");
+            int keyProgram = Integer.parseInt(programs[0].trim());
+            for (String programString : programs[1].split(",")) {
+                int program = Integer.parseInt(programString.trim());
+                map.computeIfAbsent(keyProgram, v -> new HashSet<>());
+                map.get(keyProgram).add(program);
+                map.computeIfAbsent(program, v -> new HashSet<>());
+                map.get(program).add(keyProgram);
+            }
+        }
+        return map;
+    }
+
+}
