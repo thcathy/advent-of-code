e0fe21bc098d19b5578bd2b645915dcbb65835b9
thcathy <thcathy@gmail.com>
2023-12-21 19:46:23 +0800

    complete 2023 day 14

--- src/main/java/com/adventofcode/year2023/Day14Part2.java
+++ src/main/java/com/adventofcode/year2023/Day14Part2.java
@@ -1,23 +1,16 @@
 package com.adventofcode.year2023;
 
-import static com.adventofcode.year2023.Day14Part2.Direction.NORTH;
-import static com.adventofcode.year2023.Day14Part2.Direction.SOUTH;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
+import java.io.IOException;
+import java.util.*;
+
+import static com.adventofcode.year2023.Day14Part2.Direction.*;
 
 public class Day14Part2 {
     Logger log = LoggerFactory.getLogger(Day14Part2.class);
@@ -31,33 +24,48 @@ public class Day14Part2 {
     void run() throws IOException {
         var lines = Resources.readLines(ClassLoader.getSystemResource(inputFile), Charsets.UTF_8);
         var puzzle = new Day14Part2().parseInput(lines);
-        var result = totalLoad(allRockSlideToNorth(puzzle), puzzle.height);
+        var result = totalLoadAfterSpin(puzzle, 1000000000);
         log.warn("What is the total load on the north support beams? {}", result);
     }
+    
+    long totalLoadAfterSpin(Puzzle puzzle, long runCycle) {
+        populateCheckPositionsInSequence(puzzle.width, puzzle.height);
+        var hashedCycleMap = new HashMap<String, Long>();
+        var cycleToPuzzleMap = new HashMap<Long, Puzzle>();
 
-    Set<Position> allRockSlideToNorth(Puzzle puzzle) {
-        var slidedRocks = new HashSet<Position>();
-        for (int y = 0; y < puzzle.height; y++) {
-            for (int x = 0; x < puzzle.width; x++) {
-                var position = new Position(x, y);
-                if (puzzle.roundRocks.contains(position)) {
-                    slidedRocks.add(slide(puzzle, slidedRocks, position, NORTH));
-                }
+        for (long i = 1; i <= runCycle; i++) {
+            puzzle = spin(puzzle);
+            String roundRocksHash = puzzle.getRoundRocksHash();
+
+            // pattern repeat
+            if (hashedCycleMap.containsKey(roundRocksHash)) {
+                var loopStartCycle = hashedCycleMap.get(roundRocksHash);
+                var loopSize = i - loopStartCycle;
+                var remainingCycleFromTheEnd = (runCycle - loopStartCycle) % loopSize;
+                puzzle = cycleToPuzzleMap.get(loopStartCycle + remainingCycleFromTheEnd);
+                break;
             }
+            hashedCycleMap.put(roundRocksHash, i);
+            cycleToPuzzleMap.put(i, puzzle);
         }
-        return slidedRocks;
+        return totalLoad(puzzle.roundRocks, puzzle.height);
     }
 
-
+    Puzzle spin(Puzzle puzzle) {
+        puzzle = slideRocks(puzzle, NORTH);
+        puzzle = slideRocks(puzzle, WEST);
+        puzzle = slideRocks(puzzle, SOUTH);
+        return slideRocks(puzzle, EAST);
+    }
 
     Puzzle slideRocks(Puzzle puzzle, Direction direction) {
         var slidedRocks = new HashSet<Position>();
         for (var position : checkPositionsInSequence.get(direction)) {
             if (puzzle.roundRocks.contains(position)) {
-                slidedRocks.add(slide(puzzle, slidedRocks, position, direction))
+                slidedRocks.add(slide(puzzle, slidedRocks, position, direction));
             }
         }
-        return new Puzzle();
+        return new Puzzle(puzzle.cubeRocks, slidedRocks, puzzle.width, puzzle.height);
     }
 
     Map<Direction, List<Position>> checkPositionsInSequence = new HashMap<>();
@@ -113,6 +121,10 @@ public class Day14Part2 {
         boolean isValid(Position position) {
             return position.x >= 0 && position.x < width && position.y >= 0 && position.y < height;
         }
+
+        String getRoundRocksHash() {
+            return roundRocks.stream().map(r -> r.x + "," + r.y).reduce("", (s1, s2) -> s1 + ":" + s2);
+        }
     }
 
     record Position(int x, int y) {
@@ -155,7 +167,7 @@ public class Day14Part2 {
     public void unitTest() throws IOException {        
         var lines = Resources.readLines(ClassLoader.getSystemResource("2023/day14_test.txt"), Charsets.UTF_8);
         var puzzle = new Day14Part2().parseInput(lines);
-        var result = totalLoad(allRockSlideToNorth(puzzle), puzzle.height);
-        Assert.assertEquals(136, result);
+        var result = totalLoadAfterSpin(puzzle, 1000000000);
+        Assert.assertEquals(64, result);
     }
 }
